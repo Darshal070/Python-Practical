87aece3525ecae925af3bb1aa54350c890735e31
Darshal <dhadgedarshal@gmail.com>
2026-06-20 09:10:33 +0530

    Add files via upload

---
+++ darshal4.py
@@ -0,0 +1,23 @@
+#IF-Statement pro
+i=10
+if(i>20):
+   print("10 is less than 20")
+print("we are not in if")
+
+#if else statement
+i=10
+if(i>20):
+   print("i is less than 20")
+else:
+   print("i is greater than 25")
+
+#Nested if statement
+i=10
+if(i==10):
+    if(i<20):
+        print("i is smaller than 20")
+
+        if(i<15):
+            print("i is smaller than 15")
+    else:
+        print("i is grater than 15")
