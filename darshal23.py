0fac63fd10846ee65a784a4f758df96c9a408c4f
Darshal <dhadgedarshal@gmail.com>
2026-06-26 16:59:07 +0530

    Add files via upload

---
+++ darshal23.py
@@ -0,0 +1,18 @@
+class Student:
+    def _init_(self, name, marks):
+        self.name = name          
+        self.__marks = marks     
+    def get_marks(self):
+        return self.__marks
+
+    def set_marks(self, marks):
+        if marks >= 0:
+            self.__marks = marks
+        else:
+            print("Invalid marks")
+
+s1 = Student("Darshal", 85)
+print("Name:", s1.name)
+print("Marks:", s1.get_marks())
+s1.set_marks(90)
+print("Updated Marks:", s1.get_marks())
