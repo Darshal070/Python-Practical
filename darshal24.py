096d81fe1ec4e8ede7e5d5e32baffa936b2f545c
Darshal <dhadgedarshal@gmail.com>
2026-06-27 07:33:26 +0530

    Add files via upload

---
+++ darshal24.py
@@ -0,0 +1,23 @@
+# Multiple Inheritance
+
+class Father:
+    def show_father(self):
+        print("This is Father class")
+
+
+class Mother:
+    def show_mother(self):
+        print("This is Mother class")
+
+
+class Child(Father, Mother):
+    def show_child(self):
+        print("This is Child class")
+
+
+# Create object
+obj = Child()
+
+obj.show_father()
+obj.show_mother()
+obj.show_child()
