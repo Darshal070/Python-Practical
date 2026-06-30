92c6d9c351cedd0b0ac42df2790dadcf7f7bb4ba
Darshal <dhadgedarshal@gmail.com>
2026-06-30 08:05:19 +0530

    Add files via upload

---
+++ darshalGUI27.py
@@ -0,0 +1,12 @@
+# Import Tkinter package
+import tkinter as tk
+
+# Create main window
+root = tk.Tk()
+
+# Set window title
+root.title("GUI - Darshal")
+
+# Set window size
+root.geometry("500x500")
+root.mainloop()
