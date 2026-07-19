ff0b51f6d08278a9315a019dfcf01aad57d966c3
Darshal <dhadgedarshal@gmail.com>
2026-07-19 07:34:09 +0530

    Add files via upload

---
+++ main16.py
@@ -0,0 +1,13 @@
+# main.py
+# Import the user-defined module
+import math
+
+# Take input from user
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+# Use functions from module
+print("Addition:", math.add(a, b))
+print("Subtraction:", math.subtract(a, b))
+print("Multiplication:", math.multiply(a, b))
+print("Division:", math.divide(a, b))
